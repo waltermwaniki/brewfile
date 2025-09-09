d4cb69e99369d456216b7bf118055cdf9b898310
GitHub Action <action@github.com>
2025-09-09 02:06:03 +0000

    Update formula for v0.1.4 (PyPI release)

--- Formula/brewfile.rb
+++ Formula/brewfile.rb
@@ -4,8 +4,8 @@ class Brewfile < Formula
   desc "Intelligent Homebrew package management using brew bundle with JSON configuration"
   homepage "https://github.com/waltermwaniki/homebrew-brewfile"
   # Using URL just for Homebrew metadata - actual install from PyPI
-  url "https://files.pythonhosted.org/packages/source/b/brewfile/brewfile-0.1.1.tar.gz"
-  sha256 "f728917a2a874edbd0485c16a4c74aea20ed516dade46d699ad6b5881bac21c8"
+  url "https://files.pythonhosted.org/packages/source/b/brewfile/brewfile-0.1.4.tar.gz"
+  sha256 "a03ef3861d95b329ff788495b4568c17a549d1f38481ca447ad528714ffc3c14"
   license "MIT"
   head "https://github.com/waltermwaniki/homebrew-brewfile.git", branch: "main"
 
