2cf5a16998d45b79b064ce96e38eb180aa658d5c
Walter Mwaniki <walter.g.mwaniki@gmail.com>
2025-09-15 11:34:51 -0700

    Update brewfile.rb

--- Formula/brewfile.rb
+++ Formula/brewfile.rb
@@ -5,7 +5,8 @@ class Brewfile < Formula
   homepage "https://github.com/waltermwaniki/homebrew-brewfile"
   # Using URL just for Homebrew metadata - actual install from PyPI
   url "https://files.pythonhosted.org/packages/source/b/brewfile/brewfile-0.1.5.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  # sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "fa8db2476a6f61da6468617a7bc5a882dc0680f3761c7bd80809415afd5c55fb"
   license "MIT"
   head "https://github.com/waltermwaniki/homebrew-brewfile.git", branch: "main"
 
